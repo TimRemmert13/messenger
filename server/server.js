cca830bffbac81561984bea22d22788f92b3fbac
timremmert <tbr23@scarletmail.rutgers.edu>
2018-03-30 11:14:22 -0400

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const publicPath = path.join(__dirname, '../public');
 
 let app = express();
 
-app.use(express.static(PublicPath));
+app.use(express.static(publicPath));
 
 app.listen(port, ()  => {
   console.log(`server up on port ${port}`);
